8298b1c5e6916b12a2d2bac15ce412468347467b
Juan Esteban Rodríguez Céspedes <juan.rodriguez-ce@mail.escuelaing.edu.co>
2026-04-29 16:23:08 -0500

    Scaffolding listo

---
+++ src/main/java/edu/eci/patriciaM12/PatriciaM12Application.java
@@ -0,0 +1,13 @@
+package edu.eci.patriciaM12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatriciaM12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatriciaM12Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/patriciaM12/PatriciaM12ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.patriciaM12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatriciaM12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
